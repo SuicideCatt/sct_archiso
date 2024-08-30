0eceb791f1f83206441593c0a8c3a575d0deee9b
SuicideCatt <fr7g31@gmail.com>
2024-08-30 22:10:58 +0300

    fix key init fail

--- airootfs/root/init_pacman.sh
+++ airootfs/root/init_pacman.sh
@@ -4,5 +4,10 @@ sed -i "s/#ParallelDownloads = 12/ParallelDownloads = $(nproc)/g" \
 	/etc/pacman.conf
 
 pacman-key --init
-pacman -Sy --noconfirm
+
+
+KEY=DDF7DB817396A49B2A2723F7403BD972F75D9D76
+pacman-key -r "$KEY"
+pacman-key --lsign-key "$KEY"
+
 pacman -Sy archlinux-keyring --noconfirm
