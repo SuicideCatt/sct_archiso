95c93962f2b676fd15b19504231140180d9b6933
SuicideCatt <fr7g31@gmail.com>
2024-09-02 18:44:11 +0300

    change meta data

--- profiledef.sh
+++ profiledef.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="sct_archiso"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
+iso_publisher="SuicideCatt <https://github.com/SuicideCatt>"
 iso_application="Arch Linux Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
