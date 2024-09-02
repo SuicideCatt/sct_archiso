3f87c942b040ff6c043e54d757c4cbf2417b8f49
SuicideCatt <fr7g31@gmail.com>
2024-09-02 18:49:56 +0300

    fix keyring

--- airootfs/root/init_pacman.sh
+++ airootfs/root/init_pacman.sh
@@ -4,6 +4,7 @@ sed -i "s/ParallelDownloads = .*/ParallelDownloads = $(nproc)/g" \
 	/etc/pacman.conf
 
 pacman-key --init
+pacman-key --populate archlinux
 
 KEY=DDF7DB817396A49B2A2723F7403BD972F75D9D76
 pacman-key -r "$KEY"
