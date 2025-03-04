7d9d72e59e2b75b368139853a3a05cfdd8dcf18c
SuicideCatt <random.cat.from.internet@gmail.com>
2025-03-04 12:03:17 +0200

    update install script

--- airootfs/root/install_arch.sh
+++ airootfs/root/install_arch.sh
@@ -1,13 +1,15 @@
 set -e
 
-pacstrap -K $@ base base-devel pkgs/linux-zen zfs-linux-zen linux-firmware \
+pacstrap -K $@ base base-devel \
+	pkgs/linux-zen pkgs/linux-zen-headers zfs-dkms linux-firmware \
 	neovim git networkmanager yay
 
 echo copy ~/post_install to "$1/"
 cp -r ~/post_install "$1/"
 
 echo copy pacman conf
-cat /etc/pacman.conf | head -n -6 > "$1/etc/pacman.conf"
+sed "s/#IgnorePkg   =/IgnorePkg    = linux-zen linux-zen-headers/g" \
+	/etc/pacman.conf | head -n -6 > "$1/etc/pacman.conf"
 
 echo "> arch-chroot \"$1\""
 arch-chroot "$1"
